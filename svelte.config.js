f47b45cefac41a4393c30160942eb936af24b825
Danny <danny@dfeller.xyz>
2024-07-07 23:56:02 -0400

    This sucks so bad. Documentation says nothing on this

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,8 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
+    appDir: "app", // Required as the default is _app
+
     adapter: adapter({
       // default options are shown. On some platforms
       // these options are set automatically — see below
