f5aca50276ceaec43e5a3d3228deda753fca8f9b
Danny <danny@dfeller.xyz>
2024-07-09 16:59:24 -0400

    Add theme component

---
+++ src/lib/Themes.svelte
@@ -0,0 +1,33 @@
+<!-- --background-color: #eedacc;
+--search-background-color: #ffffff;
+--search-border-color: #ccc;
+--search-focused-color: #0f0fbb;
+--search-button-color: #bd0404;
+--search-button-hover-color: #bd0404;
+--shadow-color: #0000001a;
+--item-text-color: #0f0fbb;
+--item-number-color: #000000;
+--unit-text-color: #bd0404;
+--list-background-color: #ffffff;
+--list-border-color: #ccc;
+--spinner-color: #0000001a;
+--spinner-accent-color: #3437db; -->
+
+<script>
+  export const defaultTheme = {
+    "--background-color": "#eedacc;",
+    "--search-background-color": "#ffffff;",
+    "--search-border-color": "#ccc;",
+    "--search-focused-color": "#0f0fbb;",
+    "--search-button-color": "#bd0404;",
+    "--search-button-hover-color": "#bd0404;",
+    "--shadow-color": "#0000001a;",
+    "--item-text-color": "#0f0fbb;",
+    "--item-number-color": "#000000;",
+    "--unit-text-color": "#bd0404;",
+    "--list-background-color": "#ffffff;",
+    "--list-border-color": "#ccc;",
+    "--spinner-color": "#0000001a;",
+    "--spinner-accent-color": "#3437db;",
+  };
+</script>
